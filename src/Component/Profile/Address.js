1d4550fe6574aff380dc4d478a9c6a7802f66454
RADHIARH <radhia.rahmani@sesame.com.tn>
2021-11-24 23:47:03 +0100

    send react component app to github

--- src/Component/Profile/Address.js
+++ src/Component/Profile/Address.js
@@ -2,13 +2,14 @@ import React from "react";
 function Address (){
     return (
         <div className="add">
-            <h2> Adresse</h2>
+            <h2> Adresse
+            </h2>
             <div className="address1">
-            <h4>Gouvernorat : Ariana</h4>
-            <h4>Délégation : Raoud</h4>
-            <h4>Localité : Cité Elgazala1</h4>
-            <h4>Code Postale : 2083</h4>
-        </div>
+                <h4>Gouvernorat : Ariana</h4>
+                <h4>Délégation : Raoud</h4>
+                <h4>Localité : Cité Elgazala1</h4>
+                <h4>Code Postale : 2083</h4>
+            </div>
         </div>
     );
 }
--- src/Component/Profile/Profile.js
+++ src/Component/Profile/Profile.js
@@ -2,11 +2,9 @@ import React from "react";
 import ProfilePicture from './profile.jpg'
 function Profile(){
     return (
-<div>
-<img src={ProfilePicture}></img>
-
-</div>
-
+            <div>
+                <img src={ProfilePicture}></img>
+            </div>
     );
 }
 export default Profile;
